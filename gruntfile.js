2babe44d7be0445879182c4babb13e6636205df4
Adriano Oliveira <127135267+AAROliveira@users.noreply.github.com>
2023-09-18 20:45:10 -0300

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true
+            },
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': ['src/scripts/main.js', 'src/scripts/main2.js'] 
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less', 'uglify']);
+};
 
